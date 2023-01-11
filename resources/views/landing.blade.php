5bdf91c3e26c66fb42e0837393177e127759ea6a
liyan <lianpmu@webmail.umm.ac.id>
2023-01-11 23:22:24 +0700

    slicing footer dan perbaiki slider

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -28,7 +28,7 @@
             <div class="image">
                 <img src="{{asset('img/logo.png')}}" style="width: 64px; height: 64px; margin-right: 10px; margin-bottom: 10px;" class="img-circle elevation-2" alt="User Image">
             </div>
-            <a class="navbar-brand fw" style="font-family:Cormorant; color: #EDBB62;" href="#page-top">PERPUSTAKAAN <br> STIDKI AL HAMIDY</a>
+            <a class="navbar-brand" style="font-family:Cormorant; color: #EDBB62;" href="#page-top">PERPUSTAKAAN <br> STIDKI AL HAMIDY</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 Menu
                 <i class="bi-list"></i>
@@ -61,64 +61,53 @@
                     </div>
                 </div>
                 <div class="col-lg-6">
-                    <!-- Masthead device mockup feature-->
-                    <div class="masthead-device-mockup">
-                        <!-- slider image -->
-                        <div id="carouselExampleCaptions" class="carousel slide col-l-6" data-ride="carousel">
-                            <ol class="carousel-indicators">
-                                <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-                                <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-                                <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-                            </ol>
-                            <div class="carousel-inner">
-                                <div class="carousel-item active">
-                                    <img src="{{asset('img/buku.png')}}" class="d-block w-100" alt="buku" style="border-radius: 48px;">
-                                   
-                                </div>
-                                <div class="carousel-item">
-                                    <img src="{{asset('img/coba.png')}}" class="d-block w-100" alt="..." style="border-radius: 48px;">
-                                   
-                                </div>
-                                <div class="carousel-item ">
-                                    <img src="{{asset('img/coba2.png')}}" class="d-block w-100" alt="..." style="border-radius: 48px;">
-                                    
-                                </div>
+                    <!-- slider image -->
+                    <div id="carouselExampleIndicators" class="carousel slide col-l-6" data-ride="carousel" style="left: 50px;">
+                        <ol class="carousel-indicators">
+                            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                            <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                        </ol>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="{{asset('img/buku.png')}}" class="d-block w-100" alt="buku" style="border-radius:48px;">
                             </div>
-                            <button class="carousel-control-prev" type="button" data-target="#carouselExampleCaptions" data-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Previous</span>
-                            </button>
-                            <button class="carousel-control-next" type="button" data-target="#carouselExampleCaptions" data-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Next</span>
-                            </button>
+                            <div class="carousel-item">
+                                <img src="{{asset('img/coba.png')}}" class="d-block w-100" alt="..." style="border-radius: 48px;">
+                            </div>
+                            <div class="carousel-item ">
+                                <img src="{{asset('img/coba2.png')}}" class="d-block w-100" alt="..." style="border-radius: 48px;">
+                            </div>
                         </div>
-                        <!-- card gambar-->
-                        <div class="cardbackground">
+                        <button class="carousel-control-prev" type="button" data-target="#carouselExampleIndicators" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-target="#carouselExampleIndicators" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </button>
+                    </div>
+                    <!-- card gambar-->
+                    <div class="cardbackground">
                         <div class="cardlogo text-left text-muted " style="font-size: small; text-align: center; ">
                             <img class="imagecard " src="{{asset('img/logo card.png')}}" alt="logo">
                             <p class="teks"><br>Temukan banyak Buku dan Kitab dengan cara yang mudah</p>
-                            </div>
                         </div>
-                        <div class="cardbackground2" >
-                            <div class="cardlogo text-left text-muted " style="font-size: small; text-align: center; ">
+                    </div>
+                    <div class="cardbackground2">
+                        <div class="cardlogo text-left text-muted " style="font-size: small; text-align: center; ">
                             <img class="imagecard " src="{{asset('img/logo card2.png')}}" alt="logo">
                             <p class="teks"><br>Dapatkan banyak sumber literasi yang ingin anda temukan</p>
-                            </div>
                         </div>
-                        <!--tutup card gambar -->
-                        <!-- tutup slider -->
-
                     </div>
+                    <!--tutup card gambar -->
+                    <!-- tutup slider -->
                 </div>
             </div>
-        </div>
-        </div>
-        </div>
-        </div>
     </header>
     <!-- Quote/testimonial aside-->
-<aside class="text-center bg-gradient-primary-to-secondary">
+    <aside class="text-center bg-gradient-primary-to-secondary">
         <div class="container px-5">
             <div class="row gx-5 justify-content-center">
                 <div class="col-xl-9">
@@ -136,7 +125,7 @@
                     <h3>E-Perpustakaan</h3>
                     <h2>MEMUDAHKAN AKSES LITERASI ANDA</h2>
                     <p>E-Perpustakaan adalah sebuah perpustakaan yang memungkinkan penggunanya untuk mengakses koleksi buku, jurnal, dan dokumen lainnya secara digital melalui internet. Fungsi utama dari e-perpustakaan adalah menyediakan akses ke sumber-sumber informasi yang dapat diakses secara online.</p>
-                    <button class="button">Menuju E-Perpustakaan  <i class="bi bi-arrow-right"></i></button>
+                    <button class="button">Menuju E-Perpustakaan <i class="bi bi-arrow-right"></i></button>
                 </div>
             </div>
         </div>
@@ -149,7 +138,7 @@
                     <h3>E-Repository</h3>
                     <h2>Lorem ipsum dolor sit AMET</h2>
                     <p>Lorem ipsum dolor sit amet consectetur. Arcu dictum imperdiet leo ultrices turpis at a. Risus venenatis tellus ut sit mollis nisi odio. Diam turpis enim magna fringilla hendrerit sed. In viverra sagittis eu libero. Habitasse rutrum elit semper mauris.</p>
-                    <button class="button">Menuju E-Repository  <i class="bi bi-arrow-right"></i></button>
+                    <button class="button">Menuju E-Repository <i class="bi bi-arrow-right"></i></button>
                 </div>
                 <img class="col-4 offset-1 col-sm" src="{{asset('img/perpus2.png')}}" alt="perpus">
             </div>
@@ -159,20 +148,19 @@
     <!-- Footer-->
     <footer class="bg-gradient-primary-to-secondary py-5">
         <div class="container gx-5">
-        <div class="row"> 
-        <div class="col-6">
-        <img class="logokecil" src="{{asset('img/logo.png')}}"
-            <a class="textfooter " style="font-family:Cormorant; color: #EDBB62;">PERPUSTAKAAN <br> STIDKI AL HAMIDY</a>
-          
-         
-                <div class="mb-2">&copy; 2023 Perpustakaan STIDKI AL HAMIDY </div>
-                <a href="#!">Terms</a>
-                <span class="mx-1">&middot;</span>
-                <a href="#!">Privacy</a>
+            <div class="row">
+                <div class="col-12">
+                    <img class="logokecil" src="{{asset('img/logo.png')}}">
+                    <p class="footer-text">PERPUSTAKAAN <br> STIDKI AL HAMIDY</p>
+                    <a style="color:#F9F9ED; font-style: normal;">Lorem ipsum dolor sit amet consectetur. Nulla ut sed dictumst non. Odio massa quam sed at varius nibh. Libero donec sollicitudin mollis eu. Non velit adipiscing tempus sit egestas non facilisis. Vestibulum amet.<br><br></a>
+                    <i class="bi bi-geo-alt-fill" style="color: #F9F9ED;"><a class="text-location"> Potoan, Patoan Dajah, Kec. Palengaan, Kabupaten Pamekasan, Jawa Timur 69362</a><br></i>
+                    <i class="bi bi-telephone-fill" style="color: #F9F9ED; font-family: 'Poppins';font-style: normal;"> +62823-3706-3767</i>
+                    <br><br>
+                    <hr style="color: #F9F9ED;">
+                    <a style="color: #F9F9ED;font-family: 'Poppins';font-style: normal;">&copy; 2023 Perpustakaan STIDKI AL HAMIDY </a>
+                    <a href="#!" style="float: right; color: #F9F9ED;font-family: 'Poppins';font-style: normal;">Terms • Privacy</a>
 
-
-        </div>
-        
+                </div>
             </div>
         </div>
     </footer>
