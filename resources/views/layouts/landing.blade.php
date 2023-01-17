78118538c5466aa7ecd0403000da8aa8d1d2d4a3
liyan <lianpmu@webmail.umm.ac.id>
2023-01-17 10:14:18 +0700

    menyesuaikan konsep laravel

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -72,18 +72,15 @@
         <h1 style="font-family: 'Cormorant';text-transform: uppercase;color: #2C3633;"><br>Dapatkan informasi <br>dengan artikel kami</h1>
     </div>
 
-
-
-        
    
             <div class="news-carousel owl-carousel owl-theme">
                @foreach ($news as $news)
                     <div class="card mx-2 m-sm-2 col-12" style="width: 18rem; border-radius: 20px; width: 379px; color:#FFFFFF;">
                         <img class="card-img-top img-fluid mt-2 rounded" style="width: 379px;height: 230px;" src="{{ url('/img/'.$news->file) }}">
-                        <div class="card-body">
-                            <h5 class="card-title" style="color: #2C3633;">{{ ($news->judul) }}</h5>
-                            <p class="card-text" style="color: #4C605B;">{{ ($news->deskripsi) }}</p>
-                            <a class="buttonberita btn" style="border: 1px solid #E7C377; font-family: 'Poppins';font-style: normal;color: #E7C377;" href="http://127.0.0.1:8000/artikel">Baca Selengkapnya</a>
+                        <div class="card-body text-center">
+                            <h5 class="card-title text-start" style="color: #2C3633;">{{ ($news->judul) }}</h5>
+                            <p class="card-text text-start" style="color: #4C605B;">{{ ($news->deskripsi) }}</p>
+                            <a class="buttonberita btn " style="border: 1px solid #E7C377; font-family: 'Poppins';font-style: normal;color: #E7C377;" href="http://127.0.0.1:8000/artikel">Baca Selengkapnya</a>
                         </div>
                     </div>
                     @endforeach
